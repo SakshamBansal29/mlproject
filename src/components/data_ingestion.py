234f4e67acb4553a280871edafba17ad271f97f4
SakshamBansal29 <sakshambansal.ubs@gmail.com>
2023-08-31 17:56:27 +0530

    Training with Hypparameter Tuning

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -7,8 +7,6 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
-from src.components.data_transformation import DataTransformation, DataTransformationConfig
-from src.components.model_trainer import ModelTrainer, ModelTrainerConfig
 
 @dataclass
 class DataIngestionConfig:
@@ -47,16 +45,5 @@ class DataIngestion:
             raise CustomException(e, sys)
 
 
-if __name__ == '__main__':
-    obj = DataIngestion()
-    train_data_path, test_data_path = obj.initiate_data_ingestion()
 
-    data_transformation = DataTransformation()
-    train_arr, test_arr, _ = data_transformation.initiate_data_transformation(train_data_path, test_data_path)
-
-    modeltrainer = ModelTrainer()
-    r_square = modeltrainer.initiate_model_trainer(train_arr, test_arr)  
-
-    print(r_square)
-        
     
